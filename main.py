5fd3775dabca26761236abebd7878955d19ae5a8
Ruben van Dijkhuizen <rubenvandijkhuizen@Rubens-MacBook-Air-2.local>
2023-05-16 22:03:08 +0200

    added question creating endpoint (/wiki)

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def ocr():
 
     wikiLinks = dbpedia(text, language)
 
-    gptPrompt = f'Provide a summary on the following text: \n "{text}"'
+    gptPrompt = f'Provide a summary on the following text: \n{text}'
 
     gptResponse = chatgpt(
         "You are a service that provides short summaries for for studying purposes",
@@ -33,7 +33,54 @@ def ocr():
 
     return res
     
+@app.route('/wiki', methods=["POST"])
+def wiki():
 
+    text = request.form.get("text")
+
+    gptPrompt = f'Provide questions for the following text: \n{text}'
+
+    gptBehaviour = "You are a service that provides 3 multiple choice questions based on a given text Provide 3 possible answers to each question of which only one is correct. Also provide the correct answer"
+
+    response = chatgpt(gptBehaviour, gptPrompt)
+
+    lines = response.split("\n")
+
+    questions = {
+        "question_1": {
+            "question": lines[0].split(".")[1],
+            "A": lines[1].split(") ")[1],
+            "B": lines[2].split(") ")[1],
+            "C": lines[3].split(") ")[1],
+            "correctAnswer": lines[4].split()[2][:1],
+            "correctAnswerExpl": lines[4].split(")")[1]
+        },
+        "question_2": {
+            "question": lines[6].split(".")[1],
+            "A": lines[7].split(") ")[1],
+            "B": lines[8].split(") ")[1],
+            "C": lines[9].split(") ")[1],
+            "correctAnswer": lines[10].split()[2][:1],
+            "correctAnswerExpl": lines[10].split(")")[1]
+        },
+        "question_3": {
+            "question": lines[12].split(".")[1],
+            "A": lines[13].split(") ")[1],
+            "B": lines[14].split(") ")[1],
+            "C": lines[15].split(") ")[1],
+            "correctAnswer": lines[16].split()[2][:1],
+            "correctAnswerExpl": lines[16].split(")")[1]
+        }
+    }
+
+    return questions
+
+
+
+    
+    
+
+    
 
 def dbpedia(text, language):
     
