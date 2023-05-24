3555bc4a057cd035810aa50c279ce5215e4026f9
Ruben van Dijkhuizen <rubenvandijkhuizen@Rubens-MacBook-Air-2.local>
2023-05-24 12:12:53 +0200

    Improved GPT JSON response

--- main.py
+++ main.py
@@ -67,44 +67,12 @@ def questions():
     text = request.form.get("text")
 
     # OpenAI
-    gptPrompt = f'Provide questions for the following text: \n{text}'
+    gptPrompt = f'Provide questions for the following text in JSON format: \n{text}'
     gptBehaviour = "You are a service that provides 3 multiple choice questions based on a given text Provide 3 possible answers to each question of which only one is correct. Also provide the correct answer"
     response = chatgpt(gptBehaviour, gptPrompt)
-
-    # Split response
-    lines = response.split("\n")
     
-    print(response)
 
-    questions = {
-        "question_1": {
-            "question": lines[0].split(".")[1],
-            "A": lines[1].split(") ")[1],
-            "B": lines[2].split(") ")[1],
-            "C": lines[3].split(") ")[1],
-            "correctAnswer": lines[4].split()[2][:1],
-            "correctAnswerExpl": lines[4].split(")")[1]
-        },
-        "question_2": {
-            "question": lines[6].split(".")[1],
-            "A": lines[7].split(") ")[1],
-            "B": lines[8].split(") ")[1],
-            "C": lines[9].split(") ")[1],
-            "correctAnswer": lines[10].split()[2][:1],
-            "correctAnswerExpl": lines[10].split(")")[1]
-        },
-        "question_3": {
-            "question": lines[12].split(".")[1],
-            "A": lines[13].split(") ")[1],
-            "B": lines[14].split(") ")[1],
-            "C": lines[15].split(") ")[1],
-            "correctAnswer": lines[16].split()[2][:1],
-            "correctAnswerExpl": lines[16].split(")")[1]
-        }
-    }
-
-    return questions
-
+    return response
 
 """
     /wiki endpoint (POST)
